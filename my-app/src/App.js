de853932910ac3df43912d29afc00c5a4484910f
lpewton <liampewton99@gmail.com>
2023-10-09 14:36:27 +0000

    Create title and unordered list

---
+++ my-app/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>My Chores</h1>
+      <ul>
+        <li>Do the laundry</li>
+        <li>Fetch tomatoes</li>
+        <li>Buy new sheets</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
